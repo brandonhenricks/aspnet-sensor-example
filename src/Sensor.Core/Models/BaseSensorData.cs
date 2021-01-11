5e72a93aa0714c60e28610d8002e7167ed5ddd1b
Brandon Henricks <brandon.henricks1984@gmail.com>
2021-01-11 13:28:29 -0500

    chore: Model Stubs

---
+++ src/Sensor.Core/Models/BaseSensorData.cs
@@ -0,0 +1,14 @@
+﻿namespace Sensor.Core.Models
+{
+    using System;
+
+    using Sensor.Core.Contracts;
+    using Sensor.Core.Enums;
+
+    public abstract class BaseSensorData : ISensorData
+    {
+        public SensorDataType DataType { get; protected set; } = SensorDataType.Other;
+
+        public DateTimeOffset TimeStamp { get; } = DateTimeOffset.UtcNow;
+    }
+}
---
+++ src/Sensor.Core/Models/SensorData.cs
@@ -0,0 +1,17 @@
+﻿namespace Sensor.Core.Models
+{
+    using Sensor.Core.Contracts;
+    using Sensor.Core.Enums;
+
+    public class SensorData<T> : BaseSensorData, ISensorData<T>
+        where T : class, new()
+    {
+        public SensorData(SensorDataType dataType, T data)
+        {
+            DataType = dataType;
+            Data = data;
+        }
+
+        public T Data { get; }
+    }
+}
